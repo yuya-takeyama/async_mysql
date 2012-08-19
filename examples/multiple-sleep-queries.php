0c5a501d0bba46a7fbcd3d30a4937e699d6df9ff
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-08-20 00:18:48 +0900

    Add example script

---
+++ examples/multiple-sleep-queries.php
@@ -0,0 +1,24 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$loop = new AMQ\EventLoop;
+
+$conn = $loop->connect('localhost');
+
+foreach (array(3, 6, 9, 1, 5, 2) as $i => $sleep) {
+    $query = $conn->query("SELECT {$i} AS `id`, SLEEP({$sleep}) AS `sleep_{$sleep}`");
+
+    $query->on('result', function ($result, $query) use ($i) {
+        echo "{$i}: ", $query->getQuery(), PHP_EOL;
+        $records = $result->fetch_all(MYSQLI_ASSOC);
+        var_dump($records);
+        echo PHP_EOL;
+    });
+
+    $query->on('error', function ($query) {
+        echo "Error: ", $query->getQuery(), PHP_EOL;
+        echo PHP_EOL;
+    });
+}
+
+$loop->run();
